951374bf7887528e5c176e892f754ca741be4cf9
Gabriel Souza <gabrielvt.souza@hotmail.com>
2021-03-23 20:46:11 -0300

    Loaders e fundamentos

---
+++ Fundamentos/dist/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const o=function(o){console.log(`Olá ${o}`)};console.log("Mudamos a configuração 2"),o("Matheus"),o("João"),o("Maria")})();
---
+++ Fundamentos/src/index.js
@@ -0,0 +1,7 @@
+import saudacao from "./saudacao.js";
+;
+console.log("Mudamos a configuração 2");
+
+saudacao("Matheus");
+saudacao("João");
+saudacao("Maria");
