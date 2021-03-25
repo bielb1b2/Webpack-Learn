4127dd0752aa6763add71ef3c4acf047a8c0bfd9
Gabriel Souza <gabrielvt.souza@hotmail.com>
2021-03-25 12:49:56 -0300

    JavaScript bundle plugin e dotenv plugin

--- Plugins/src/index.js
+++ Plugins/src/index.js
@@ -3,3 +3,7 @@ import Heading from './components/heading/heading.js';
 const heading = new Heading();
 
 heading.create('pagina principal');
+
+console.log(VERSION);
+console.log(PORT);
+console.log(process.env.API_KEY);
--- Plugins/webpack.config.js
+++ Plugins/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const webpack = require('webpack');
+const DotEnvPlugin = require('dotenv-webpack');
 
 module.exports = {
     entry: './src/index.js',
@@ -9,6 +12,10 @@ module.exports = {
     },
 
     mode: 'development',
+    optimization: {
+        minimize: true,
+        minimizer: [ new TerserPlugin()]
+    },
     module: {
         rules: [
             {
@@ -22,6 +29,11 @@ module.exports = {
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'styles.css'
-        })
+        }),
+        new webpack.DefinePlugin({
+            VERSION: JSON.stringify('1.0.2'),
+            PORT: JSON.stringify('8080')
+        }),
+        new DotEnvPlugin()
     ]
 }
